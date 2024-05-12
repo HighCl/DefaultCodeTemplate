a9862979cfeb1f092a9f5b886d973c6c917fdc26
HighCl <fdsa1469@naver.com>
2024-05-12 17:21:07 +0900

    기능 삭제: 포함되어 있던 에셋 삭제
    [24.05.12_강성욱]
    
    - 포함되어 있던 무료 에셋 삭제
    필요 시 다운받아야 함

--- Assets/02_Scripts/Etc/_Util/ReadOnlyAttribute/Scripts/ReadOnlyAttribute.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-/// <summary>
-/// Use this PropertyAttribute to make a public variable uneditable in the Inspector
-/// </summary>
-namespace DefaultSetting
-{
-    public class ReadOnlyAttribute : PropertyAttribute { }
-}
