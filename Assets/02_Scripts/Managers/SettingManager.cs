d7804fdaaf9db73a5f0c71126016648bfaec9392
HighCl <fdsa1469@naver.com>
2024-07-08 01:30:50 +0900

    기능 추가: TMP 기본값 설정 기능 추가
    [24.07.08_강성욱]
    
    - TMP 기본값 설정 기능 추가

--- Assets/02_Scripts/Managers/SettingManager.cs
+++ Assets/02_Scripts/Managers/SettingManager.cs
@@ -1,5 +1,10 @@
 using com.cyborgAssets.inspectorButtonPro;
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using TMPro;
 using UnityEditor;
 using UnityEngine;
 
@@ -7,19 +12,31 @@ namespace DefaultSetting
 {
     public class SettingManager : MonoBehaviour
     {
+        [Header("SteamWorks Settings")]
         [SerializeField] private bool isSteamWorks = false;
+
+        [Header("InputSystem Settings")]
         [SerializeField] private bool isNewInputSystem = true;
 
+        [Header("TMP Settings")]
+        [SerializeField] private bool enbaleRayCastTarget = false;
+        [Tooltip("파일명 입력 시 프로젝트 내에서 검색합니다.")]
+        [SerializeField] private string targetFont = "NanumGothicSDF";
+
+
         [ProButton]
-        public void OnSetting()
+        private void OnSetting()
         {
             Debug.Log("설정 시작");
 
             SetEnableSteamWorks();
-            SettingInputSystem();
+            SetInputSystem();
+            SetTMPs();
+
+            Debug.Log("설정 종료");
         }
 
-        public void SetEnableSteamWorks()
+        private void SetEnableSteamWorks()
         {
             string DEFINE_DISABLESTEAMWORKS = "DISABLESTEAMWORKS";
 
@@ -33,7 +50,7 @@ namespace DefaultSetting
             }
         }
 
-        public void SettingInputSystem()
+        private void SetInputSystem()
         {
             string DEFINE_NEW_INPUT_SYSTEM = "INPUT_TYPE_NEW";
             string DEFINE_LEGACY_INPUT_SYSTEM = "INPUT_TYPE_LEGACY";
@@ -47,6 +64,52 @@ namespace DefaultSetting
             {
                 AddDefineSymbol(DEFINE_LEGACY_INPUT_SYSTEM);
                 RemoveDefineSymbol(DEFINE_NEW_INPUT_SYSTEM);
+            }
+        }
+
+        private void SetTMPs()
+        {
+            TMP_Settings tmpSettings = TMP_Settings.GetSettings();
+
+            Type type = tmpSettings.GetType();
+
+            //Set enable Raycast Target
+            FieldInfo enableRaycastTargetField = type.GetField("m_EnableRaycastTarget", BindingFlags.NonPublic | BindingFlags.Instance);
+            if (enableRaycastTargetField != null)
+            {
+                enableRaycastTargetField.SetValue(tmpSettings, enbaleRayCastTarget);
+            }
+            else
+            {
+                throw new InvalidOperationException("Field not found");
+            }
+
+            //Set Default Font Asset
+            FieldInfo defaultFontAssetField = type.GetField("m_defaultFontAsset", BindingFlags.NonPublic | BindingFlags.Instance);
+            if (defaultFontAssetField != null)
+            {
+                List<string> findAssetPath = AssetDatabase.FindAssets(targetFont, new string[] { "Assets" })
+                    .Where(e => Path.GetFileNameWithoutExtension(AssetDatabase.GUIDToAssetPath(e)) == targetFont)
+                    .Select(asset => AssetDatabase.GUIDToAssetPath(asset))
+                    .ToList();
+
+                switch (findAssetPath.Count)
+                {
+                    case 0:
+                        Debug.LogError($"폰트 탐색에 실패했습니다.");
+                        break;
+                    case 1:
+                        object obj = AssetDatabase.LoadAssetAtPath(findAssetPath[0], typeof(TMP_FontAsset));
+                        defaultFontAssetField.SetValue(tmpSettings, obj);
+                        break;
+                    default:
+                        Debug.LogError($"탐색된 폰트가 여러개입니다. \n개수: {findAssetPath.Count}");
+                        break;
+                }
+            }
+            else
+            {
+                throw new InvalidOperationException("Field not found");
             }
         }
 
