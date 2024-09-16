79151177782cf0793873b79dd0ffc5ea9d2172b8
HighCl <fdsa1469@naver.com>
2024-09-17 01:36:34 +0900

    기능 추가: Vector 값 변경 확장메소드 추가-2
    [24.09.17_강성욱]
    
    - Vector2 추가
    - 람다 함수로 변경

--- Assets/02_Scripts/Etc/_Util/ExtensionMethod/Extension.cs
+++ Assets/02_Scripts/Etc/_Util/ExtensionMethod/Extension.cs
@@ -363,20 +363,12 @@ namespace DefaultSetting.Utility
         #endregion
 
         #region Vector
-        public static Vector3 SetX(this Vector3 vec, float value)
-        {
-            return new Vector3(value, vec.y, vec.z);
-        }
+        public static Vector2 SetX(this Vector2 vec, float value) => new Vector3(value, vec.y);
+        public static Vector2 SetY(this Vector2 vec, float value) => new Vector3(vec.x, value);
 
-        public static Vector3 SetY(this Vector3 vec, float value)
-        {
-            return new Vector3(vec.x, value, vec.z);
-        }
-
-        public static Vector3 SetZ(this Vector3 vec, float value)
-        {
-            return new Vector3(vec.x, vec.y, value);
-        }
+        public static Vector3 SetX(this Vector3 vec, float value) => new Vector3(value, vec.y, vec.z);
+        public static Vector3 SetY(this Vector3 vec, float value) => new Vector3(vec.x, value, vec.z);
+        public static Vector3 SetZ(this Vector3 vec, float value) => new Vector3(vec.x, vec.y, value);
         #endregion
 
         #region Color
