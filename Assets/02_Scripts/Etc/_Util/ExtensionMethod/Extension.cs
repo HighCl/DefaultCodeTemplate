e257438a3592cb180512181852e382bfd34337b4
HighCl <fdsa1469@naver.com>
2024-09-17 01:31:16 +0900

    기능 추가: Vector 값 변경 확장메소드 추가
    [24.09.17_강성욱]
    
    - Vector 값 변경 확장메소드 추가

--- Assets/02_Scripts/Etc/_Util/ExtensionMethod/Extension.cs
+++ Assets/02_Scripts/Etc/_Util/ExtensionMethod/Extension.cs
@@ -362,6 +362,23 @@ namespace DefaultSetting.Utility
         }
         #endregion
 
+        #region Vector
+        public static Vector3 SetX(this Vector3 vec, float value)
+        {
+            return new Vector3(value, vec.y, vec.z);
+        }
+
+        public static Vector3 SetY(this Vector3 vec, float value)
+        {
+            return new Vector3(vec.x, value, vec.z);
+        }
+
+        public static Vector3 SetZ(this Vector3 vec, float value)
+        {
+            return new Vector3(vec.x, vec.y, value);
+        }
+        #endregion
+
         #region Color
         public static Color GetChangeAlpha(this Color color, float value_0_1)
         {
