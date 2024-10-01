785ed8bc493cec46d8befec7a9505d328f105801
HighCl <fdsa1469@naver.com>
2024-10-02 00:21:48 +0900

    리펙토링: PhysicsExtension 분리
    [24.10.02_강성욱]
    
    - 사용 편의성을 위해 PhysicsExtension 분리

---
+++ Assets/02_Scripts/Etc/_Util/ExtensionMethod/Physics2DExtensions.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+namespace DefaultSetting
+{
+    public static class Physics2DExtensions
+    {
+        static readonly int DEFAULT_MAX_DISTANCE = 10000;
+        static readonly Color DONT_HIT_COLOR = Color.green;
+        static readonly Color HIT_COLOR = Color.red;
+        static readonly float RHOMBUS_SIZE = 0.5f;
+
+        public static RaycastHit2D RaycastWithDraw(Vector3 startPos, Vector3 dir, float maxDistance = Mathf.Infinity, int layerMask = Physics.DefaultRaycastLayers)
+        {
+            RaycastHit2D hitInfo = Physics2D.Raycast(startPos, dir, maxDistance, layerMask);
+
+#if UNITY_EDITOR
+            if (hitInfo.collider == null && maxDistance == Mathf.Infinity)
+                maxDistance = DEFAULT_MAX_DISTANCE;
+
+            Vector3 endPos = hitInfo.collider != null ? hitInfo.point : startPos + dir.normalized * maxDistance;
+            Color color = hitInfo.collider != null ? HIT_COLOR : DONT_HIT_COLOR;
+
+            Debug.DrawLine(startPos, endPos, color, Time.deltaTime);
+#endif
+
+            return hitInfo;
+        }
+
+        public static RaycastHit2D[] RaycastAllWithDraw(Vector3 startPos, Vector3 dir, float maxDistance = Mathf.Infinity, int layerMask = Physics.DefaultRaycastLayers)
+        {
+            RaycastHit2D[] hitInfoArr = Physics2D.RaycastAll(startPos, dir, maxDistance, layerMask);
+
+#if UNITY_EDITOR
+            if (hitInfoArr.Length == 0 && maxDistance == Mathf.Infinity)
+                maxDistance = DEFAULT_MAX_DISTANCE;
+
+            Vector3 endPos = startPos + dir.normalized * maxDistance;
+            Debug.DrawLine(startPos, endPos, Color.green, Time.deltaTime);
+
+            foreach (var item in hitInfoArr)
+            {
+                Vector2 left = item.point + Vector2.left * RHOMBUS_SIZE;
+                Vector2 right = item.point + Vector2.right * RHOMBUS_SIZE;
+                Vector2 up = item.point + Vector2.up * RHOMBUS_SIZE;
+                Vector2 down = item.point + Vector2.down * RHOMBUS_SIZE;
+
+                Debug.DrawLine(left, up, Color.red, Time.deltaTime);
+                Debug.DrawLine(up, right, Color.red, Time.deltaTime);
+                Debug.DrawLine(right, down, Color.red, Time.deltaTime);
+                Debug.DrawLine(down, left, Color.red, Time.deltaTime);
+                Debug.DrawLine(up, down, Color.red, Time.deltaTime);
+                Debug.DrawLine(left, right, Color.red, Time.deltaTime);
+            }
+#endif
+
+            return hitInfoArr;
+        }
+    }
+}
